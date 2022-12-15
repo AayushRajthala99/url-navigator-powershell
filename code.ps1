3a4229fc5e8a90ceac600a108f7f438c3ba9c146
Aayush Rajthala <Aayushrajthala@gmail.com>
2022-12-16 00:08:54 +0545

    Added Code Argument Handlers

--- code.ps1
+++ code.ps1
@@ -1,5 +1,15 @@
 $filePath = $args[0]
 $duration = $args[1]
+$testName = $args[2]
+
+if (($filePath.Length -eq 0) -or ($duration.Length -eq 0) -or ($testName.Length -eq 0)) {
+    $message = ''
+    if ($filePath.Length -eq 0) { $message = $message + ' <filePath>' }
+    if ($duration.Length -eq 0) { $message = $message + ' <RecordDurationinSeconds>' }
+    if ($testName.Length -eq 0) { $message = $message + ' <testName>' }
+    Write-Output $('--Missing Arguments:' + $message)
+    Exit
+}
 
 if ($duration -gt 4) {
     $screenshotTime = $duration - 1
@@ -9,13 +19,15 @@ if ($duration -gt 4) {
     $screenshotTime = $screenshotTime.ToString("hh\:mm\:ss\.ff")
 }
 else {
-    Write-Output 'Enter Time Greater Than 3 Seconds!'
-    exit
+    Write-Output 'Enter Time Greater Than 4 Seconds!'
+    Exit
 }
 
 $urls = Get-Content -Path $filePath
 
 if ($urls.Length -ne 0) {
+    
+    Write-Output '[ URL-NAVIGATOR-POWERSHELL ] Developed By Aayush Rajthala!'
     
     # Keystrokes Generation Object...
     # $wshell = New-Object -ComObject wscript.shell;
@@ -23,11 +35,12 @@ if ($urls.Length -ne 0) {
     # Timestamp, Count for Unique Identity of Files & Folders...
     $count = 1
     $timestamp = Get-Date -Format "yyyyMMddTHHmmss"
+    $directoryName = $testName + '_' + $timestamp
     
     # Directory Generation...
-    New-Item ".\recordings\$timestamp" -itemType Directory
-    # New-Item ".\responses\$timestamp" -itemType Directory
-    New-Item ".\screenshots\$timestamp" -itemType Directory
+    New-Item ".\recordings\$directoryName" -itemType Directory
+    # New-Item ".\responses\$directoryName" -itemType Directory
+    New-Item ".\screenshots\$directoryName" -itemType Directory
 
     ForEach ($url in $urls) {
         $url = $url.Trim() # Removes white/blank spaces from URLs...
@@ -35,12 +48,12 @@ if ($urls.Length -ne 0) {
         #Filename Generation Operation...
         $filename = $url.Replace('https://', '')
         $filename = $filename.Replace('http://', '')
-        $filename = $timestamp + "_" + $count + "_" + $filename.Replace('/', '_')
+        $filename = $count + "_" + $filename.Replace('/', '_')
         $count = $count + 1
 
         # Response Log Generation...
-        # curl -sSL -D ./responses/$timestamp/$filename.txt $url | Out-Null
-        # $response = Get-Content -Path .\responses\$timestamp\$filename.txt
+        # curl -sSL -D ./responses/$directoryName/$filename.txt $url | Out-Null
+        # $response = Get-Content -Path .\responses\$directoryName\$filename.txt
 
         #Flags for Accept-Ranges & Content-Disposition...
         # $arFlag = $false
@@ -64,21 +77,39 @@ if ($urls.Length -ne 0) {
         $ffmpegTitle = 'title=' + $title
 
         # Screen Record Operation...
-        ffmpeg -f gdigrab -i $ffmpegTitle -loglevel panic -t $duration -s hd1080 -aspect 16:9 -an -vcodec libx264 .\recordings\$timestamp\$filename.mp4
+        ffmpeg -f gdigrab -i $ffmpegTitle -loglevel panic -t $duration -s hd1080 -aspect 16:9 -an -vcodec libx264 .\recordings\$directoryName\$filename.mp4
 
         # Screenshot Operation...
-        ffmpeg -i .\recordings\$timestamp\$filename.mp4 -ss $screenshotTime -frames:v 1 -q:v 2 .\screenshots\$timestamp\$filename.jpeg
+        ffmpeg -i .\recordings\$directoryName\$filename.mp4 -ss $screenshotTime -frames:v 1 -q:v 2 .\screenshots\$directoryName\$filename.jpeg
 
         # # Flags Check for Downloadable-Status...
         # if ($arFlag -or $cdFlag) {
-        #     Add-Content .\responses\$timestamp\$filename.txt "`nDownloadable-Status: True"
+        #     Add-Content .\responses\$directoryName\$filename.txt "`nDownloadable-Status: True"
         # }
         # else {
-        #     Add-Content .\responses\$timestamp\$filename.txt "`nDownloadable-Status: False"
+        #     Add-Content .\responses\$directoryName\$filename.txt "`nDownloadable-Status: False"
         # }
         Stop-Process -Name chrome
+
+        $testConfirm = Read-Host -Prompt "Save Test Results? Type [CONFIRM] to CONFIRM: "
+        $testConfirm = $testConfirm.ToUpper()
+        $confirmDecision = Read-Host -Prompt "Are You Sure? [Y/N]: "
+        $confirmDecision = $confirmDecision.ToUpper()
+
+        if (($testConfirm -eq 'CONFIRM') -and ($confirmDecision -eq 'Y')) {
+            Write-Output $('--' + $testName + ' Test Completed!')
+            Exit
+        }
+        else {
+            Remove-Item ".\recordings\$directoryName" -Recurse
+            # Remove-Item ".\responses\$directoryName" -Recurse
+            Remove-Item ".\screenshots\$directoryName" -Recurse
+            Write-Output $($testName + ' Discarded!')
+            Exit
+        }
     }
 }
 else {
-    Write-Output '[urls.txt]-File-is-Empty!'
+    Write-Output $('--[ ' + $filePath + ' ]--File is Empty!!!')
+    Exit
 }
