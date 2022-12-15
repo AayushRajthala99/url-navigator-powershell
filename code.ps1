25a6c0f6c8f29776951c29d3a6a3107f9ac7ff35
Aayush Rajthala <Aayushrajthala@gmail.com>
2022-12-15 18:37:47 +0545

    Response Handling WIP

--- code.ps1
+++ code.ps1
@@ -1,21 +1,33 @@
-$urls = Get-Content -Path .\urls.txt
+$filePath = $args[0]
+$duration = $args[1]
+
+if ($duration -gt 4) {
+    $screenshotTime = $duration - 1
+    $duration = [timespan]::fromseconds($duration)
+    $duration = $duration.ToString("hh\:mm\:ss\.ff")
+    $screenshotTime = [timespan]::fromseconds($screenshotTime)
+    $screenshotTime = $screenshotTime.ToString("hh\:mm\:ss\.ff")
+}
+else {
+    Write-Output 'Enter Time Greater Than 3 Seconds!'
+    exit
+}
+
+$urls = Get-Content -Path $filePath
 
 if ($urls.Length -ne 0) {
     
     # Keystrokes Generation Object...
-    $wshell = New-Object -ComObject wscript.shell;
+    # $wshell = New-Object -ComObject wscript.shell;
     
-    # Timestamp, for Unique Identity of Files & Folders...
+    # Timestamp, Count for Unique Identity of Files & Folders...
     $count = 1
     $timestamp = Get-Date -Format "yyyyMMddTHHmmss"
     
     # Directory Generation...
     New-Item ".\recordings\$timestamp" -itemType Directory
-    New-Item ".\responses\$timestamp" -itemType Directory
+    # New-Item ".\responses\$timestamp" -itemType Directory
     New-Item ".\screenshots\$timestamp" -itemType Directory
-
-    # Browser Initialization in Incognito Mode... 
-    & 'C:\Program Files\Google\Chrome\Application\chrome.exe' --incognito --new-window --start-maximized 'chrome://newtab'
 
     ForEach ($url in $urls) {
         $url = $url.Trim() # Removes white/blank spaces from URLs...
@@ -27,49 +39,45 @@ if ($urls.Length -ne 0) {
         $count = $count + 1
 
         # Response Log Generation...
-        curl -sSL -D ./responses/$timestamp/$filename.txt $url | Out-Null
-        $response = Get-Content -Path .\responses\$timestamp\$filename.txt
+        # curl -sSL -D ./responses/$timestamp/$filename.txt $url | Out-Null
+        # $response = Get-Content -Path .\responses\$timestamp\$filename.txt
 
         #Flags for Accept-Ranges & Content-Disposition...
-        $arFlag = $false
-        $cdFlag = $false
+        # $arFlag = $false
+        # $cdFlag = $false
 
-        ForEach ($i in $response) {
-            if ($i.contains('Accept-Ranges') -and !($i.contains('Accept-Ranges: none'))) {
-                $arFlag = $true
-            }
+        # ForEach ($i in $response) {
+        #     if ($i.contains('Accept-Ranges') -and !($i.contains('Accept-Ranges: none'))) {
+        #         $arFlag = $true
+        #     }
 
-            if ($i.contains('content-disposition')) {
-                $cdFlag = $true
-            }
-        }
+        #     if ($i.contains('content-disposition')) {
+        #         $cdFlag = $true
+        #     }
+        # }
         
         Start-Sleep -Seconds 1
-        & 'C:\Program Files\Google\Chrome\Application\chrome.exe' --incognito --new-tab --start-maximized $url
+        & 'C:\Program Files\Google\Chrome\Application\chrome.exe' --incognito --new-window --start-maximized $url
         Start-Sleep -Seconds 2
         $title = (Get-Process -Name chrome | Select-Object MainWindowTitle)
         ForEach ($i in $title) { if ($i.mainWindowTitle -ne '') { $title = $i.mainWindowTitle; break; } }
         $ffmpegTitle = 'title=' + $title
 
         # Screen Record Operation...
-        ffmpeg -f gdigrab -i $ffmpegTitle -loglevel panic -t 00:00:10.00 -s hd1080 -aspect 16:9 -an -vcodec libx264 .\recordings\$timestamp\$filename.mp4
+        ffmpeg -f gdigrab -i $ffmpegTitle -loglevel panic -t $duration -s hd1080 -aspect 16:9 -an -vcodec libx264 .\recordings\$timestamp\$filename.mp4
 
         # Screenshot Operation...
-        ffmpeg -i .\recordings\$timestamp\$filename.mp4 -ss 00:00:09.50 -frames:v 1 -q:v 2 .\screenshots\$timestamp\$filename.jpeg
-        
-        $wshell.AppActivate($title) | Out-Null
-        Start-Sleep -Seconds 1
-        $wshell.SendKeys('^w') # Generates 'Ctrl + w' Keystroke...
+        ffmpeg -i .\recordings\$timestamp\$filename.mp4 -ss $screenshotTime -frames:v 1 -q:v 2 .\screenshots\$timestamp\$filename.jpeg
 
-        # Flags Check for Downloadable-Status...
-        if ($arFlag -or $cdFlag) {
-            Add-Content .\responses\$timestamp\$filename.txt "`nDownloadable-Status: True"
-        }
-        else {
-            Add-Content .\responses\$timestamp\$filename.txt "`nDownloadable-Status: False"
-        }
+        # # Flags Check for Downloadable-Status...
+        # if ($arFlag -or $cdFlag) {
+        #     Add-Content .\responses\$timestamp\$filename.txt "`nDownloadable-Status: True"
+        # }
+        # else {
+        #     Add-Content .\responses\$timestamp\$filename.txt "`nDownloadable-Status: False"
+        # }
+        Stop-Process -Name chrome
     }
-    Stop-Process -Name chrome
 }
 else {
     Write-Output '[urls.txt]-File-is-Empty!'
